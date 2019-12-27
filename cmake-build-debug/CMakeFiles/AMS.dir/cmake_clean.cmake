3a2a7f0ce9c0b25ca3c030999168d3cf55ef0b19
jokker <18761609908@163.com>
2019-12-27 14:56:35 +0800

    * 初始化

---
+++ cmake-build-debug/CMakeFiles/AMS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AMS.dir/main.cpp.obj"
+  "AMS.pdb"
+  "AMS.exe"
+  "AMS.exe.manifest"
+  "libAMS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+/*
+ * 要实现的功能
+    * 添加联系人
+    * 显示联系人
+    * 删除联系人
+    * 查找联系人
+    * 修改联系人
+    * 清空联系人
+    * 退出通讯录
+ * */
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
